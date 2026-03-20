1dc2abf7675f5180253d4fc3984e42b41834b43b
Slicer001 <theslicerforgod@gmail.com>
2026-03-20 13:37:31 -0400

    attempt 2

---
+++ keyboard_controller.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@onready var first_line_edit: LineEdit = $"../Inputs/FirstLineEdit"
+@onready var second_line_edit: LineEdit = $"../Inputs/SecondLineEdit"
+@onready var image_link_line_edit: LineEdit = $"../WebInputs/HBoxContainer/ImageLinkLineEdit"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_first_line_edit_focus_entered() -> void:
+	DisplayServer.virtual_keyboard_show(first_line_edit.text)
+
+
+func _on_first_line_edit_focus_exited() -> void:
+	DisplayServer.virtual_keyboard_hide()
